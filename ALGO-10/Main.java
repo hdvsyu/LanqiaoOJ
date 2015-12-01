181c9c3299c3d2e5ef8fa99c2f3d6a2d1b78ace7
JoyHwong <hzy961165750@qq.com>
2015-12-01 12:52:54 +0800

    ALGO-10
    集合运算

---
+++ ALGO-10/Main.java
@@ -0,0 +1,55 @@
+package test;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		TreeSet<Integer> treeSetA = new TreeSet<Integer>();
+		int n = in.nextInt();
+		for (int i = 0; i < n; i++) {
+			treeSetA.add(in.nextInt());
+		}
+		
+		TreeSet<Integer> treeSetB = new TreeSet<Integer>();
+		int m = in.nextInt();
+		for (int i = 0; i < m; i++) {
+			treeSetB.add(in.nextInt());
+		}
+		in.close();
+		
+		printDivide(treeSetA, treeSetB);
+		printAdd(treeSetA, treeSetB);
+		printRemain(treeSetA, treeSetB);
+	}
+	
+	public static void printAdd(TreeSet<Integer> a, TreeSet<Integer> b) {
+		a.addAll(b);
+		for (int i : a) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+	
+	public static void printDivide(TreeSet<Integer> a, TreeSet<Integer> b) {
+		
+		TreeSet<Integer> tempA = (TreeSet<Integer>) a.clone();
+		a.removeAll(b);
+		tempA.removeAll(a);
+		for (int i : tempA) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+	
+	public static void printRemain(TreeSet<Integer> a, TreeSet<Integer> b) {
+		a.removeAll(b);
+		for (int i : a) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+
+}
---
+++ ALGO-10/README.md
@@ -0,0 +1,2 @@
+集合运算 蓝桥OJ
+<img src="screenshot-lx.lanqiao.org 2015-11-30 21-26-33">
