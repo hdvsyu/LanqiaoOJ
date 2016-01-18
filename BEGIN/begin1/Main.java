7a52c546a2c15eb19461f818d289f03218e6d601
JoyHwong <hzy961165750@qq.com>
2016-01-18 13:58:59 +0800

    BEGIN-1

--- BEGIN/begin1/Main.java
+++ BEGIN/begin1/Main.java
@@ -1,10 +1,15 @@
 package begin1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = in.nextInt();
+		in.close();
+		System.out.println(a + b);
 	}
 
 }
