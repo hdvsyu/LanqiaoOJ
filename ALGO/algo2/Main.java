f895c25f387fa144bb6b18590f75d222aba4819a
JoyHwong <hzy961165750@qq.com>
2016-01-20 15:00:15 +0800

    ALGO-2

--- ALGO/algo2/Main.java
+++ ALGO/algo2/Main.java
@@ -1,28 +1,30 @@
 package algo2;
 
-//Get sixty percent.
-//And still have some errors.
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		Scanner in = new Scanner(System.in);
 
-		double n = in.nextDouble();
+		long n = in.nextLong();
 		in.close();
 
 		if (n <= 2) {
-			System.out.printf("%.0f", n);
-		} else if (0 == n % 2) {
+			System.out.print(n);
+			return;
+		} 
+		
+		if (0 == n % 2) {
 			// Even.
 			if (0 != n % 3) {
-				System.out.printf("%.0f", (n * (n - 1) * (n - 3)));
+				System.out.print((n * (n - 1) * (n - 3)));
 			} else {
-				System.out.printf("%.0f", ((n - 3) * (n - 2) * (n - 1)));
+				System.out.print((n - 1) * (n - 2) * (n - 3));
 			}
 		} else {
 			// Odd.
-			System.out.printf("%.0f", (n * (n - 1) * (n - 2)));
+			System.out.print(n * (n - 1) * (n - 2));
 		}
 	}
 }
