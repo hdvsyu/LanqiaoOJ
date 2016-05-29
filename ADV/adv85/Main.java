64f6c7048418f919b784fdad2ddeafa5b277500f
JoyHwong <hzy961165750@qq.com>
2016-05-29 09:14:06 +0800

    ADV-85

---
+++ ADV/adv85/Main.java
@@ -0,0 +1,18 @@
+package adv85;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = in.nextInt();
+		in.close();
+		System.out.println(a + "+" + b + "=" + (a + b));
+		System.out.println(a + "-" + b + "=" + (a - b));
+		System.out.println(a + "*" + b + "=" + (a * b));
+		System.out.println(a + "/" + b + "=" + (a / b));
+	}
+
+}
