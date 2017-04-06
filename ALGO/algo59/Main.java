601d1a3652b7ee903dd75490a185747f91549b40
JoyHwong <hzy961165750@qq.com>
2017-04-06 22:35:08 +0800

    ALGO 59

--- ALGO/algo59/Main.java
+++ ALGO/algo59/Main.java
@@ -1,23 +1,68 @@
 package algo59;
-
+// 自己写的快速排序在遇到一个极端情况，会出现超时
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		int[] num = new int[n];
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(reader.readLine());
+		num = new int[n];
 		for (int i = 0; i < n; i++) {
-			num[i] = in.nextInt();
+			num[i] = Integer.parseInt(reader.readLine());
 		}
-		in.close();
+		reader.close();
+//		quickSort();
 		Arrays.sort(num);
 		
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < num.length; i++) {
 			System.out.println(num[i]);
 		}
 	}
+	
+	private static int[] num;
 
+	private static void quickSort() {
+		quickSort(0, num.length - 1);
+	}
+	
+	private static void quickSort(int l, int r) {
+		if (l < r) {
+			int pivot = partion(l, r);
+			quickSort(l, pivot - 1);
+			quickSort(pivot + 1, r);
+		}
+	}
+	
+	private static int partion(int l, int r) {
+		swap(l, (l + r) / 2);
+		int pivot = l;		
+		while (true) {
+			while (l <= r && num[l] < num[pivot]) {
+				l++;
+			}
+			while (l <= r && num[r] > num[pivot]) {
+				r--;
+			}
+			
+			if (l < r) {
+				swap(l, r);
+				l++;
+				r--;
+			} else {
+				break;
+			}
+		}
+		swap(r, pivot);
+		return r;
+	}
+	
+	private static void swap(int i, int j) {
+		int temp = num[i];
+		num[i] = num[j];
+		num[j] = temp;
+	}
 }
